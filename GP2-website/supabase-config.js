3d52e5e7b37761761a435e8514c4baa02ad4dc6a
Lamyaa315 <lamyaalnahdi2@gmail.com>
2026-02-09 22:50:23 +0300

    need to sleep

--- GP2-website/supabase-config.js
+++ GP2-website/supabase-config.js
@@ -3,4 +3,4 @@ const SUPABASE_URL = "https://mtxungnwzittdxnfdhvd.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_pPnHK3uF-_roE6wmByPKxw_-kt5gpgr";
 
 //  Supabase
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
